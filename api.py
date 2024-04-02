ef8fa6300a80aa5177041d045e4ae816e5f7561c
Sergey <sergsmile29@gmail.com>
2024-04-03 00:52:51 +0300

    feat(app): Add class Birthday and logic with errors

---
+++ api.py
@@ -0,0 +1,145 @@
+from collections import UserDict
+from datetime import datetime
+
+
+def command_parser(input_str: str):
+    """Parse command and attributes from user input
+
+    Args:
+        input_str (str): All string what user input
+
+    Returns:
+        command, *args: command - main operation, args - list of attributes for functions like [name, phone, new_phone]
+    """
+    try:
+        command, *args = input_str.lower().split()
+        return command, *args
+    except (TypeError, ValueError):
+        print("Command is empty string...Try again!")
+
+
+def greetings():
+    return "Welcome to CLI assistant 🔥"
+
+
+def help_api():
+    return (
+        "Available commands: \n"
+        "- add name number\n"
+        "- update name old_number new_number\n"
+        "- delete name\n"
+        "- all (saw all contacts)\n"
+    )
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f"{self.value}"
+
+
+class Birthday(Field):
+    def __init__(self, value):
+        if not self.is_valid_date_format(value):
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+
+        self.value = datetime.strptime(value, "%d.%m.%Y")
+
+    def is_valid_date_format(self, value):
+        try:
+            datetime.strptime(value, "%d.%m.%Y")
+            return True
+        except ValueError:
+            return False
+
+
+class Name(Field): ...
+
+
+class Phone(Field): ...
+
+
+class Record:
+    """Record - class for new user, where we take all info: name and phones
+
+    Main methods:
+
+    phone_is_exist : Find exist phone in record
+    edit_phone : Edit one of the phone in phones
+    add_phone : Add phone to phones list
+
+    """
+
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def add_birthday(self, value):
+        print(value)
+        self.birthday = Birthday(value)
+
+    @staticmethod
+    def phone_is_exist(data, phone):
+        for p in data:
+            if p.value == phone:
+                return True
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                print(f"Phone {old_phone} edited to {new_phone}")
+                break
+        else:
+            print(f"Phone {old_phone} is not exist!")
+
+    def add_phone(self, phone):
+        if self.phone_is_exist(self.phones, phone):
+            return print("This number already exist")
+
+        self.phones.append(Phone(phone))
+
+    def __str__(self):
+        return f"Contact name: {self.name.value} \nPhones: {'; '.join(p.value for p in self.phones)}\nBirthday: {self.birthday.value.strftime("%d.%m.%Y")}\n"
+
+
+class AddressBook(UserDict):
+    """AddressBook - class where we hold info about all users.
+
+    Main methods:
+
+    add_record: Add new record to book
+    find_record: Find the record inside book
+    show: Show all data from the book
+    remove: Remove record from book
+
+
+    Args:
+        UserDict (dict): OptimizedDict from collections for holding data inside class
+    """
+
+    def add_record(self, record):
+        self.data[record.name] = record
+
+    def find_record(self, target):
+        for i in self.data.values():
+            if i.name.value.lower() == target.lower():
+                return i
+
+    def show(self):
+        if len(self.data) == 0:
+            print("No data exist!")
+        for record in self.data.values():
+            print(record)
+
+    def remove(self, target):
+        for record in list(self.data.keys()):
+            if record.value == target:
+                del self.data[record]
+                print(f"User {target} has been deleted!")
---
+++ main.py
@@ -0,0 +1,84 @@
+from api import Record, AddressBook, greetings, help_api, command_parser
+
+
+def main():
+    book = AddressBook()
+    exit_commands = ["q", "quit", "exit", "leave", "left"]
+    print('Welcome to assistant bot! You can use "help" command to see more')
+    while True:
+        params = input(">>>  ")
+        # if str is empty - start new loop
+        if not params:
+            print("Please, enter not empty string! ")
+            continue
+
+        # get main command and attributes
+        command, *args = command_parser(params)
+        if len(args) != 0:
+            name = args[0]
+
+        match (command):
+            case "hello" | "start":
+                print(greetings())
+
+            case "add" | "create":
+                if len(args) < 2:
+                    print(f'Please enter, correct args. Example: "add Oleh 0932244555"')
+                    continue
+                new_contact = Record(name)
+                exist_record = book.find_record(name)
+                if exist_record:
+                    exist_record.add_phone(args[1])
+                else:
+                    new_contact.add_phone(args[1])
+                    book.add_record(new_contact)
+
+            case "find" | "get":
+                exist = book.find_record(name)
+                print(exist)
+
+            case "add_birthday":
+                if len(args) < 2:
+                    print(
+                        f'Please enter, correct args. Example: "add_birthday Oleh 22.03.2024"'
+                    )
+                    continue
+                exist_record = book.find_record(name)
+                if exist_record:
+                    exist_record.add_birthday(args[1])
+                else:
+                    print("User is not exist")
+                    continue
+
+            case "update":
+                if len(args) < 3:
+                    print(
+                        f'Please enter, correct args. Example: "update Oleh 0932244555 05012345678"'
+                    )
+                    continue
+                exist = book.find_record(name)
+                if exist:
+                    exist.edit_phone(args[1], args[2])
+                else:
+                    print(f'User "{name}" is not found')
+
+            case "delete" | "del":
+                book.remove(name)
+
+            case "all" | "show":
+                book.show()
+
+            case "help" | "info":
+                print(help_api())
+
+            # if main command was in exit commands list - exit
+            case command if command in exit_commands:
+                print("Bye!")
+                break
+            # if ony attribute is not match
+            case _:
+                print("Invalid command! Try again...\n")
+
+
+if __name__ == "__main__":
+    main()
